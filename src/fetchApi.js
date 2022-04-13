e8135285576287d8c0f67e6959ed5a247a7a619e
sainingo <nickmentor421@gmail.com>
2022-04-13 13:04:44 +0300

    Remove unused code

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,11 +1,5 @@
 const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/llFawVoYD7zLAfvY9jQn/scores/';
 
-// const fetchApi = async () => {
-//   const res = await fetch(apiUrl);
-//   const result = res.json();
-//   return result;
-// };
-
 const postApi = (score) => {
   const scoreResult = fetch(apiUrl, {
     method: 'POST',
